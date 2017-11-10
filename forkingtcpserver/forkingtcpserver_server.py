63e595a51dc5899cb2424685b545fb6737957e13
kidnohr2000 <kaide@zero-start.jp>
2017-11-10 18:40:01 +0900

    socketserver.TCPServerの使用

--- forkingtcpserver/forkingtcpserver_server.py
+++ forkingtcpserver/forkingtcpserver_server.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # vim:tabstop=4:shiftwidth=4:expandtab
 
+# -*- coding: utf-8 -*-
+# vim:tabstop=4:shiftwidth=4:expandtab
+
 import logging
 import time
 import socketserver
@@ -108,10 +111,40 @@ class EchoServer(socketserver.TCPServer):
 
 
 if __name__ == '__main__':
+    import socket
+    import threading
 
     address = ('localhost', 10000)  # let the kernel assign a port
     server = EchoServer(address, EchoRequestHandler)
-    try:
-        server.serve_forever()
-    finally:
-        server.socket.close()
+    ip, port = server.server_address  # what port was assigned?
+
+    # Start the server in a thread
+    t = threading.Thread(target=server.serve_forever)
+    t.setDaemon(True)  # don't hang on exit
+    t.start()
+
+    logger = logging.getLogger('client')
+    logger.info('Server on %s:%s', ip, port)
+
+    # Connect to the server
+    logger.debug('creating socket')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logger.debug('connecting to server')
+    s.connect((ip, port))
+
+    # Send the data
+    message = '5'.encode()
+    logger.debug('sending data: %r', message)
+    len_sent = s.send(message)
+
+    # Receive a response
+    logger.debug('waiting for response')
+    response = s.recv(len_sent)
+    logger.debug('response from server: %r', response)
+
+    # Clean up
+    server.shutdown()
+    logger.debug('closing socket')
+    s.close()
+    logger.debug('done')
+    server.socket.close()
