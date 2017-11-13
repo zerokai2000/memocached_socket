4202ab23e10af273947ec5293bd478ba6131d389
kidnohr2000 <kaide@zero-start.jp>
2017-11-13 11:46:08 +0900

    selectorsの追加

---
+++ selectors/selectors_client.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# vim:tabstop=4:shiftwidth=4:expandtab
+
+import socket
+import sys
+import selectors
+
+# Create a TCP/IP socket
+message = sys.argv[1]
+outgoing = message.split()
+mysel = selectors.DefaultSelector()
+keep_running = True
+
+bytes_sent = 0
+bytes_received = 0
+
+# Connecting is a blocking operation, so call setblocking()
+# after it returns.
+server_address = ('localhost', 10000)
+print('connecting to {} port {}'.format(*server_address))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(server_address)
+sock.setblocking(False)
+
+try:
+
+    # Set up the selector to watch for when the socket is ready
+    # to send data as well as when there is data to read.
+    mysel.register(
+        sock,
+        selectors.EVENT_READ | selectors.EVENT_WRITE,
+    )
+    while keep_running:
+        print('waiting for I/O')
+        for key, mask in mysel.select(timeout=1):
+            connection = key.fileobj
+            client_address = connection.getpeername()
+            print('client({})'.format(client_address))
+
+            if mask & selectors.EVENT_READ:
+                print('  ready to read')
+                data = connection.recv(1024)
+                if data:
+                    # A readable client socket has data
+                    print('  received {!r}'.format(data))
+                    bytes_received += len(data)
+
+                # Interpret empty result as closed connection,
+                # and also close when we have received a copy
+                # of all of the data sent.
+                keep_running = not (
+                    data or
+                    (bytes_received and
+                     (bytes_received == bytes_sent))
+                )
+
+            if mask & selectors.EVENT_WRITE:
+                print('  ready to write')
+                if not outgoing:
+                    # We are out of messages, so we no longer need to
+                    # write anything. Change our registration to let
+                    # us keep reading responses from the server.
+                    print('  switching to read-only')
+                    mysel.modify(sock, selectors.EVENT_READ)
+                else:
+                    # Send the next message.
+                    next_msg_str = outgoing.pop()
+                    next_msg = next_msg_str.encode()
+                    print('  sending {!r}'.format(next_msg))
+                    sock.sendall(next_msg)
+                    bytes_sent += len(next_msg)
+
+finally:
+    print('shutting down')
+    mysel.unregister(connection)
+    connection.close()
+    mysel.close()
